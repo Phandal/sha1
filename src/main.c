d9aaca6f47fb1f398f8bb435596f976fd20d21ca
Phandal <bfeld.111@gmail.com>
2026-08-18 15:55:14 -0500

    sha1 hash working

--- src/main.c
+++ src/main.c
@@ -5,22 +5,24 @@
 
 #define SHA1_BLOCK_SIZE 64
 
-#define SHA1_ROTATE(x, n) (x << n | x >> (n - 1))
-
 typedef enum {
   SHA1_OK = 0,
   SHA1_NULL,
+  SHA1_SIZE,
 } sha1_err;
 
 typedef struct {
   int block_idx;
   uint8_t block[SHA1_BLOCK_SIZE];
   uint32_t hash[5];
+  uint64_t message_length;
 } sha1_ctx_t;
 
 sha1_err sha1_init(sha1_ctx_t *ctx);
 sha1_err sha1_feed(sha1_ctx_t *ctx, const uint8_t *data, size_t len);
 sha1_err sha1_digest(sha1_ctx_t *ctx, uint8_t digest[20]);
+uint32_t _sha1_rotate(uint32_t x, int n);
+sha1_err _sha1_pad_block(sha1_ctx_t *ctx);
 sha1_err _sha1_compress_block(sha1_ctx_t *ctx);
 
 sha1_err sha1_init(sha1_ctx_t *ctx) {
@@ -29,6 +31,7 @@ sha1_err sha1_init(sha1_ctx_t *ctx) {
   }
 
   ctx->block_idx = 0;
+  ctx->message_length = 0;
 
   ctx->hash[0] = 0X67452301;
   ctx->hash[1] = 0XEFCDAB89;
@@ -55,6 +58,12 @@ sha1_err sha1_feed(sha1_ctx_t *ctx, const uint8_t *data, size_t len) {
     }
 
     ctx->block[ctx->block_idx++] = data[x];
+    ctx->message_length += 8;
+
+    // Integer Overflow
+    if (ctx->message_length == 0) {
+      return SHA1_SIZE;
+    }
   }
 
   return SHA1_OK;
@@ -64,6 +73,8 @@ sha1_err sha1_digest(sha1_ctx_t *ctx, uint8_t digest[20]) {
   if (!ctx) {
     return SHA1_NULL;
   }
+
+  _sha1_pad_block(ctx);
 
   for (int i = 0; i < 5; ++i) {
     digest[i * 4] = ctx->hash[i] >> 24;
@@ -76,7 +87,7 @@ sha1_err sha1_digest(sha1_ctx_t *ctx, uint8_t digest[20]) {
 }
 
 sha1_err _sha1_compress_block(sha1_ctx_t *ctx) {
-  uint32_t K[4] = { 0X5A827999, 0X6ED9EBA1, 0X8F1BBCDC, 0XCA62C1D6 };
+  uint32_t K[4] = {0X5A827999, 0X6ED9EBA1, 0X8F1BBCDC, 0XCA62C1D6};
   uint32_t W[80];
   int i;
   uint32_t temp, a, b, c, d, e;
@@ -93,7 +104,7 @@ sha1_err _sha1_compress_block(sha1_ctx_t *ctx) {
   // Load the rest of the words in the W array with a formula
   // W[i] = ROTL(W[i-3] XOR W[i-8] XOR W[i-14] XOR W[i-16], 1)
   for (i = 16; i < 80; ++i) {
-      W[i] = SHA1_ROTATE(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1);
+    W[i] = _sha1_rotate(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
   }
 
   // 2. Setup the initial working vars
@@ -105,37 +116,37 @@ sha1_err _sha1_compress_block(sha1_ctx_t *ctx) {
 
   // 3. Run 80 rounds of compression on the word array
   for (i = 0; i < 20; ++i) {
-    temp = SHA1_ROTATE(a, 5) + ((b & c) | ((~b) & d)) + e + W[i] + K[0];
+    temp = _sha1_rotate(a, 5) + ((b & c) | ((~b) & d)) + e + W[i] + K[0];
     e = d;
     d = c;
-    c = SHA1_ROTATE(b, 30);
+    c = _sha1_rotate(b, 30);
     b = a;
     a = temp;
   }
 
   for (i = 20; i < 40; ++i) {
-    temp = SHA1_ROTATE(a, 5) + (b ^ c ^ d) + e + W[i] + K[1];
+    temp = _sha1_rotate(a, 5) + (b ^ c ^ d) + e + W[i] + K[1];
     e = d;
     d = c;
-    c = SHA1_ROTATE(b, 30);
+    c = _sha1_rotate(b, 30);
     b = a;
     a = temp;
   }
 
   for (i = 40; i < 60; ++i) {
-    temp = SHA1_ROTATE(a, 5) + ((b & c) | (b & d) | (c & d)) + e + W[i] + K[2];
+    temp = _sha1_rotate(a, 5) + ((b & c) | (b & d) | (c & d)) + e + W[i] + K[2];
     e = d;
     d = c;
-    c = SHA1_ROTATE(b, 30);
+    c = _sha1_rotate(b, 30);
     b = a;
     a = temp;
   }
 
   for (i = 60; i < 80; ++i) {
-    temp = SHA1_ROTATE(a, 5) + (b ^ c ^ d) + e + W[i] + K[3];
+    temp = _sha1_rotate(a, 5) + (b ^ c ^ d) + e + W[i] + K[3];
     e = d;
     d = c;
-    c = SHA1_ROTATE(b, 30);
+    c = _sha1_rotate(b, 30);
     b = a;
     a = temp;
   }
@@ -145,8 +156,45 @@ sha1_err _sha1_compress_block(sha1_ctx_t *ctx) {
   ctx->hash[2] += c;
   ctx->hash[3] += d;
   ctx->hash[4] += e;
-  
+
   ctx->block_idx = 0;
+  return SHA1_OK;
+}
+
+uint32_t _sha1_rotate(uint32_t x, int n) { return (x << n) | (x >> (32 - n)); }
+
+sha1_err _sha1_pad_block(sha1_ctx_t *ctx) {
+  if (ctx->block_idx < 55) {
+    ctx->block[ctx->block_idx++] = 0x80;
+
+    while (ctx->block_idx < 56) {
+      ctx->block[ctx->block_idx++] = 0;
+    }
+  } else {
+    ctx->block[ctx->block_idx++] = 0x80;
+
+    while (ctx->block_idx < 64) {
+      ctx->block[ctx->block_idx++] = 0;
+    }
+
+    _sha1_compress_block(ctx);
+
+    while (ctx->block_idx < 56) {
+      ctx->block[ctx->block_idx++] = 0;
+    }
+  }
+
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 56;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 48;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 40;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 32;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 24;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 16;
+  ctx->block[ctx->block_idx++] = ctx->message_length >> 8;
+  ctx->block[ctx->block_idx++] = ctx->message_length;
+
+  _sha1_compress_block(ctx);
+
   return SHA1_OK;
 }
 
@@ -156,7 +204,6 @@ int main(void) {
   uint8_t digest[20];
   size_t len;
   uint8_t data[512];
-  // uint8_t data[3] = {'a', 'b', 'c'};
 
   err = sha1_init(&ctx);
   if (err != SHA1_OK) {
@@ -178,29 +225,11 @@ int main(void) {
     return 1;
   }
 
-  printf("Digest:\n");
+  // printf("Digest:\n");
   for (int i = 0; i < 20; ++i) {
-    printf("%0X", digest[i]);
+    printf("%02x", digest[i]);
   }
   printf("\n");
 
   return 0;
 }
-
-/*
-1. Pad messge (append byte 0x80, fill with zeros, last 8 bytes are for the
-original message length)
-2. Split into 512 bit (64 byte) blocks, then do 3-5 for each block
-3. Initialize the 5 hash state variables to their initialization vector
-4. Split block into 16 'Words' (2 Bytes or 16 bits), then complete 80 round of
-compression with chaing F and K every 20 rounds 4a. Expand the 'Word' into 180
-bits with the fomula W[i] = ROTL(W[i-3] XOR W[i-8] XOR W[i-14] XOR W[i-16], 1)
-
-
-| Rounds | Logical Function f(b, c, d)         | Constant K | Function Name |
-|——–-----|——————————---------------------------|——————------|—————----------|
-| 0–19   | (b AND c) OR ((NOT b) AND d)        | 0x5A827999 | Choice (Ch)   |
-| 20–39  | b XOR c XOR d                       | 0x6ED9EBA1 | Parity        |
-| 40–59  | (b AND c) OR (b AND d) OR (c AND d) | 0x8F1BBCDC | Majority (Maj)|
-| 60–79  | b XOR c XOR d                       | 0xCA62C1D6 | Parity        |
-*/
